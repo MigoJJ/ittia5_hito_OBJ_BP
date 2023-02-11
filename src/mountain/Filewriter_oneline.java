f0a6d73ecb05ac06c47806209940df57e8bda22d
migoey <migoey@migoey-vm>
2023-02-11 23:56:06 +0900

    read_write_file

--- src/mountain/Filewriter_oneline.java
+++ src/mountain/Filewriter_oneline.java
@@ -8,34 +8,36 @@ public class Filewriter_oneline {
 	 static String homeDir = System.getProperty("user.home");
   public static void main(String str) {
     try {
-      FileWriter writer = new FileWriter(homeDir +"/ittia1_chart/addjar_ittia5/hitto/bp/bpoutput",true);
-//      FileWriter writer = new FileWriter(homeDir +"/ittia1_chart/addjar_ittia5/hitto/bp/bpoutput",true);
+      FileWriter writer = new FileWriter(Bpmain.fhomeDir,true);
+//      FileWriter writer = new FileWriter(Bpmain.fhomeDir",true);
       writer.write(str+"\n");
       writer.close();
       System.out.println("String written to file.");
+      read_write_file(null);
     } catch (IOException e) {
       System.out.println("An error occurred while writing to file.");
       e.printStackTrace();
     }
+
   }
-//    public static void read_write_file(String[] args) {
-//      try {
-//        FileReader reader = new FileReader("input.txt");
-//        FileWriter writer = new FileWriter("output.txt");
-//
-//        int c;
-//        while ((c = reader.read()) != -1) {
-//          writer.write(c);
-//        }
-//
-//        reader.close();
-//        writer.close();
-//
-//        System.out.println("File copied.");
-//      } catch (IOException e) {
-//        System.out.println("An error occurred while copying the file.");
-//        e.printStackTrace();
-//      }
-//    }
+    public static void read_write_file(String[] args) {
+      try {
+        FileReader reader = new FileReader(Bpmain.fhomeDir);
+        FileWriter writer = new FileWriter(homeDir + "/ittia1_chart/je/panse/doro/text/samsara/6OBJ");
+
+        int c;
+        while ((c = reader.read()) != -1) {
+          writer.write(c);
+        }
+
+        reader.close();
+        writer.close();
+
+        System.out.println("File copied.");
+      } catch (IOException e) {
+        System.out.println("An error occurred while copying the file.");
+        e.printStackTrace();
+      }
+    }
   }
 
